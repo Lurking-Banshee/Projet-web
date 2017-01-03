af59062c859e6d630b6b7e28d1d8e2171385c577
Lurking-Banshee <amiot.clelie@gmail.com>
2017-01-03 14:17:14 +0100

    modifs mineures
    img par defaut dans résultats, suppression éléments navbar test

--- storage/cache/f4/f4a16e07832ba93931154919060d31bac73a9ad8178e5f923728c7c899294942.php
+++ storage/cache/f4/f4a16e07832ba93931154919060d31bac73a9ad8178e5f923728c7c899294942.php
@@ -34,30 +34,40 @@ class __TwigTemplate_f7a7e49997c0185a7612c49f3c350c8bd6c334920546b1d928f00780e34
             echo "            <div class=\"col s12 m6 l3\">
                 <div class=\"card sticky-action medium\">
                     <div class=\"card-image waves-effect waves-block waves-light\">
-                        <img class=\"activator\" src=\"https://image.tmdb.org/t/p/original";
+\t\t\t\t\t\t";
             // line 13
-            echo twig_escape_filter($this->env, $this->getAttribute($context["serie"], "poster_path", array()), "html", null, true);
-            echo "\">
-                    </div>
+            if ( !twig_test_empty($this->getAttribute($context["serie"], "poster_path", array()))) {
+                // line 14
+                echo "\t\t\t\t\t\t\t<img class=\"activator\" src=\"https://image.tmdb.org/t/p/original";
+                echo twig_escape_filter($this->env, $this->getAttribute($context["serie"], "poster_path", array()), "html", null, true);
+                echo "\">
+\t\t\t\t\t\t";
+            } else {
+                // line 16
+                echo "\t\t\t\t\t\t\t<img class=\"activator\" src=\"http://www.ztackey.com/wp-content/uploads/2016/11/placeholder-1.jpg\">
+\t\t\t\t\t\t";
+            }
+            // line 18
+            echo "                    </div>
                     <div class=\"card-content\">
                         <span class=\"card-title activator\">";
-            // line 16
+            // line 20
             echo twig_escape_filter($this->env, $this->getAttribute($context["serie"], "name", array()), "html", null, true);
             echo "<i class=\"material-icons right\">more_vert</i></span>
                     </div>
                     <div class=\"card-reveal\">
                         <span class=\"card-title grey-text text-darken-4\">";
-            // line 19
+            // line 23
             echo twig_escape_filter($this->env, $this->getAttribute($context["serie"], "name", array()), "html", null, true);
             echo "<i class=\"material-icons right\">close</i></span>
                         <p>";
-            // line 20
+            // line 24
             echo twig_escape_filter($this->env, $this->getAttribute($context["serie"], "overview", array()), "html", null, true);
             echo "</p>
                     </div>
                     <div class=\"card-action\">
                         <a href=\"/show/";
-            // line 23
+            // line 27
             echo twig_escape_filter($this->env, $this->getAttribute($context["serie"], "id", array()), "html", null, true);
             echo "\"><span class=\"red-text text-darken-1\">Explorer</span></a>
                         <a class=\"waves-effect waves-light btn red darken-1\"><i class=\"material-icons left\">add</i>Ajouter</a>
@@ -68,39 +78,15 @@ class __TwigTemplate_f7a7e49997c0185a7612c49f3c350c8bd6c334920546b1d928f00780e34
             $context['_iterated'] = true;
         }
         if (!$context['_iterated']) {
-            // line 29
+            // line 33
             echo "\t\t\t<span>Aucune série correspondant aux critères trouvée.</span>\t
         ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['serie'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 31
-        echo "\t</div>
-\t
-\t";
-        // line 33
-        echo twig_escape_filter($this->env, twig_length_filter($this->env, (isset($context["series"]) ? $context["series"] : null)), "html", null, true);
-        echo "
-\t";
-        // line 34
-        if ((twig_length_filter($this->env, (isset($context["series"]) ? $context["series"] : null)) > 20)) {
-            // line 35
-            echo "\t<div class=\"row\">
-\t\t<ul class=\"pagination center-align\">
-\t\t\t<li class=\"disabled\"><a href=\"#!\"><i class=\"material-icons\">chevron_left</i></a></li>
-\t\t\t<li class=\"active\"><a href=\"#!\">1</a></li>
-\t\t\t<li class=\"waves-effect\"><a href=\"#!\">2</a></li>
-\t\t\t<li class=\"waves-effect\"><a href=\"#!\">3</a></li>
-\t\t\t<li class=\"waves-effect\"><a href=\"#!\">4</a></li>
-\t\t\t<li class=\"waves-effect\"><a href=\"#!\">5</a></li>
-\t\t\t<li class=\"waves-effect\"><a href=\"#!\"><i class=\"material-icons\">chevron_right</i></a></li>
-\t\t  </ul>       
-    </div>
-\t";
-        }
-        // line 47
-        echo "\t
+        // line 35
+        echo "\t</div>\t
 </div>";
     }
 
@@ -116,7 +102,7 @@ class __TwigTemplate_f7a7e49997c0185a7612c49f3c350c8bd6c334920546b1d928f00780e34
 
     public function getDebugInfo()
     {
-        return array (  103 => 47,  89 => 35,  87 => 34,  83 => 33,  79 => 31,  72 => 29,  61 => 23,  55 => 20,  51 => 19,  45 => 16,  39 => 13,  34 => 10,  29 => 9,  19 => 1,);
+        return array (  89 => 35,  82 => 33,  71 => 27,  65 => 24,  61 => 23,  55 => 20,  51 => 18,  47 => 16,  41 => 14,  39 => 13,  34 => 10,  29 => 9,  19 => 1,);
     }
 }
 /* <div class="container">*/
@@ -131,7 +117,11 @@ class __TwigTemplate_f7a7e49997c0185a7612c49f3c350c8bd6c334920546b1d928f00780e34
 /*             <div class="col s12 m6 l3">*/
 /*                 <div class="card sticky-action medium">*/
 /*                     <div class="card-image waves-effect waves-block waves-light">*/
-/*                         <img class="activator" src="https://image.tmdb.org/t/p/original{{ serie.poster_path }}">*/
+/* 						{% if serie.poster_path is not empty %}*/
+/* 							<img class="activator" src="https://image.tmdb.org/t/p/original{{ serie.poster_path }}">*/
+/* 						{% else %}*/
+/* 							<img class="activator" src="http://www.ztackey.com/wp-content/uploads/2016/11/placeholder-1.jpg">*/
+/* 						{% endif %}*/
 /*                     </div>*/
 /*                     <div class="card-content">*/
 /*                         <span class="card-title activator">{{ serie.name }}<i class="material-icons right">more_vert</i></span>*/
@@ -149,21 +139,5 @@ class __TwigTemplate_f7a7e49997c0185a7612c49f3c350c8bd6c334920546b1d928f00780e34
 /* 		{% else %}*/
 /* 			<span>Aucune série correspondant aux critères trouvée.</span>	*/
 /*         {% endfor %}*/
-/* 	</div>*/
-/* 	*/
-/* 	{{series|length}}*/
-/* 	{% if series|length > 20 %}*/
-/* 	<div class="row">*/
-/* 		<ul class="pagination center-align">*/
-/* 			<li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>*/
-/* 			<li class="active"><a href="#!">1</a></li>*/
-/* 			<li class="waves-effect"><a href="#!">2</a></li>*/
-/* 			<li class="waves-effect"><a href="#!">3</a></li>*/
-/* 			<li class="waves-effect"><a href="#!">4</a></li>*/
-/* 			<li class="waves-effect"><a href="#!">5</a></li>*/
-/* 			<li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>*/
-/* 		  </ul>       */
-/*     </div>*/
-/* 	{% endif %}*/
-/* 	*/
+/* 	</div>	*/
 /* </div>*/
